838cb906cb753559ee881fe5bdbcaf99a5c408cf
ABaig <abaig@kenmarkitan.com>
2022-04-09 18:14:28 +0530

    Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  maptileKey: 'SoL71Zyf7SmLrVYWC7fQ',
+  smartStyle: 'https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key='
 };
